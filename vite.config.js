7923f56954f4f8c14f78cf23dc563b2879d17a07
vbhops <wickedc6@gmail.com>
2025-09-18 15:48:27 -0500

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// If deploying to https://<USERNAME>.github.io/<REPO>/
-// replace <REPO> with your repository name below.
-export default defineConfig({
-  plugins: [react()],
-  base: '/volleyball_resume_site/', 
-})
